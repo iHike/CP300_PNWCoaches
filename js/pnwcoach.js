f041cabe526a1b1c734a362a81c1785fa818197f
Bruce Perry <bruce@iperry.net>
2015-05-28 09:27:46 -0700

    20150528 Pre-Work Commit

--- js/pnwcoach.js
+++ js/pnwcoach.js
@@ -13,38 +13,68 @@ var car = [];
       [0,1,2,3,4,5,6,7,8,9,10,11]
   ];
 
+// Call PHP for SQL Call
+
+$.ajax({ url: 'apps/endTheTrendDBCall.php',
+ data: {action: 'test'},
+ dataType: 'json',
+ type: 'post',
+ success: function(output) {
+   
+   var $coachName = output[0];
+   var $coachCity = output[1];
+   var $coachStory = output[2];
+   var $coachStatement = output[3];
+   var $coachPic = output[4];
+   
+//              alert($coachPic);
+   
+    document.getElementById("coachPhoto").src = $coachPic;
+//    document.getElementById("coachCity").innerHTML = json.coachesJ[ coachNum ].coachCity;
+//    document.getElementById("coachName").innerHTML = json.coachesJ[ coachNum ].coachName;
+//    document.getElementById("coachStory").innerHTML = json.coachesJ[ coachNum ].coachStory;
+//    document.getElementById("coachStatement").innerHTML = json.coachesJ[ coachNum ].coachStatement;
+
+   
+//   $parsedJSON = JSON.parse(output);
+//   
+//   echo "$parsedJSON[1)";
+   
+//   [["One","A"],["Two","B"],["Three","C"],["Four","D"]]
+          }
+ });
 
 // Read in JSON File
 
-$.getJSON( coachJSON, function( json ) {
-
-  console.log("Coach JSON: " + json.coachesJ[ coachNum ].coachUse);
-  
-  document.getElementById("coachPhoto").src = json.coachesJ[ coachNum ].coachPic;
-  document.getElementById("coachCity").innerHTML = json.coachesJ[ coachNum ].coachCity;
-  document.getElementById("coachName").innerHTML = json.coachesJ[ coachNum ].coachName;
-  document.getElementById("coachStory").innerHTML = json.coachesJ[ coachNum ].coachStory;
-  document.getElementById("coachStatement").innerHTML = json.coachesJ[ coachNum ].coachStatement;
-  
-});
+//$.getJSON( coachJSON, function( json ) {
+//
+//  console.log("Coach JSON: " + json.coachesJ[ coachNum ].coachUse);
+//  
+//  document.getElementById("coachPhoto").src = json.coachesJ[ coachNum ].coachPic;
+//  document.getElementById("coachCity").innerHTML = json.coachesJ[ coachNum ].coachCity;
+//  document.getElementById("coachName").innerHTML = json.coachesJ[ coachNum ].coachName;
+//  document.getElementById("coachStory").innerHTML = json.coachesJ[ coachNum ].coachStory;
+//  document.getElementById("coachStatement").innerHTML = json.coachesJ[ coachNum ].coachStatement;
+//  
+//});
 
 
 // The Coach Button & While Loop
 
-function theCoach() {
-
-  $.getJSON( coachJSON, function( json ) {
-
-    console.log("Coach Function");
-    i = 0;
-
-//    while ((json.coachesJ[ i ].coachUse) > 0) {
-//        text += "<br>The Coach has been Used: " + (json.coachesJ[ i ].coachUse);
-//        i = i + 1;
-//    }
-
-  document.getElementById("coach").innerHTML = "Coach JSON: " + json.coachesJ[ coachNum ].coachUse;
-  });
+//function theCoach() {
+//
+//  $.getJSON( coachJSON, function( json ) {
+//
+//    console.log("Coach Function");
+//    i = 0;
+//
+////    while ((json.coachesJ[ i ].coachUse) > 0) {
+////        text += "<br>The Coach has been Used: " + (json.coachesJ[ i ].coachUse);
+////        i = i + 1;
+////    }
+//
+//  document.getElementById("coach").innerHTML = "Coach JSON: " + json.coachesJ[ coachNum ].coachUse;
+//  });
   
 };
 
