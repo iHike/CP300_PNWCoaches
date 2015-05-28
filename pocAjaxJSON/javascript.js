2a1f185f572a5268034bef178034b27b75027c6c
Bruce Perry <bruce@iperry.net>
2015-05-28 08:58:02 -0700

    20150528 Updates for JSON -> Javascript POC Working

--- pocAjaxJSON/javascript.js
+++ pocAjaxJSON/javascript.js
@@ -3,7 +3,19 @@ $.ajax({ url: 'php.php',
  dataType: 'json',
  type: 'post',
  success: function(output) {
-              alert(output);
+   
+   var $one = output[0];
+   var $two = output[1];
+   var $three = output[2];
+   var $four = output[3];
+   
+              alert($four);
+   
+//   $parsedJSON = JSON.parse(output);
+//   
+//   echo "$parsedJSON[1)";
+   
+//   [["One","A"],["Two","B"],["Three","C"],["Four","D"]]
           }
  });
 
--- pocAjaxJSON/php.php
+++ pocAjaxJSON/php.php
@@ -1,15 +1,59 @@
 <?php
 
-$cJSONString = "";
+//$cJSONString = "";
 
 function JSONArray() {
   
-    $cPHPArray = array("AAA", "BBB", "CCC", "DDD", "EEE", "FFF");
-    $cJSONArray = json_encode($cPHPArray);
-    echo "$cJSONArray";
+$cars = array
+   (
+   array("Tom"),
+   array("Frank"),
+   array("Bob"),
+   array("Andrew")
+   );
+   
+//echo $cars[0][0].": In stock: ".$cars[0][1].".<br>";
+//echo $cars[1][0].": In stock: ".$cars[1][1].".<br>";
+//echo $cars[2][0].": In stock: ".$cars[2][1].".<br>";
+//echo $cars[3][0].": In stock: ".$cars[3][1].".<br>";
+
+$cJSONArray = json_encode($cars);
+echo "$cJSONArray";
   
+//  
+//    $cPHPArray = array
+//  (
+//  array("One","AAA"),
+//  array("One","AAA"),
+//  array("One","AAA"),
+//  array("One","AAA")
+//  );
+//      
+////  $cPHPArray = array("One" => "AAA", "Two" => "BBB", "Three" => "CCC", "Four" => "DDD", "Five" => "EEE", "Six" : "FFF");
+//  
+//  echo $PHPArray[1][1];
+//  
+////    $cJSONArray = json_encode($cPHPArray);
+////    echo "$cJSONArray";
+//  
 }
 
-JSONArray(); // call the function
+JSONArray();
+
+//$cars = array
+//   (
+//   array("One"),
+//   array("Two"),
+//   array("Three"),
+//   array("Four")
+//   );
+//   
+////echo $cars[0][0].": In stock: ".$cars[0][1].".<br>";
+////echo $cars[1][0].": In stock: ".$cars[1][1].".<br>";
+////echo $cars[2][0].": In stock: ".$cars[2][1].".<br>";
+////echo $cars[3][0].": In stock: ".$cars[3][1].".<br>";
+//
+//$cJSONArray = json_encode($cars);
+//echo "$cJSONArray";
 
 ?>
